2a710e3144acc828a287579c3960d545678b0b77
unknown <vinayMsuresh>
2022-03-21 13:00:42 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useAlert } from 'react-alert'; 
 import './App.css';
 
 function App() {
+  const alert = useAlert();
+  const fun = () =>{
+    alert.error("This is an alert with extra actions!", {
+      title: "Alert with extra actions!",
+      actions: [
+        {
+          copy: "Do something",
+        },
+      ],
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={fun}>Click here</button>
     </div>
   );
 }
